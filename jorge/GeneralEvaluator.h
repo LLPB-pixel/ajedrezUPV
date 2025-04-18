210500c117c1bfa85e832f3796e82c6a69f81d9d
LLPB-pixel <141754913+LLPB-pixel@users.noreply.github.com>
2025-04-18 12:08:20 +0200

    Update GeneralEvaluator.h

--- jorge/GeneralEvaluator.h
+++ jorge/GeneralEvaluator.h
@@ -2,13 +2,13 @@
 #define GENERALEVALUATOR_H
 
 #include "Evaluator.h"
-
+using namespace chess;
 class GeneralEvaluator : public Evaluator {
 public:
   GeneralEvaluator();
 
   float evaluate(const Board *board, const Color color) override;
-  float position(const Board *board, const Color color) override;
+  float positionOfThePiecesAndMaterial(const chess::Board *board);
   float material(const Board *board, const Color color) override;
   float pawn_structure(const Board *board, const Color color) override;
   float safe_king(const Board *board, const Color color) override;
@@ -18,6 +18,7 @@ public:
   private:
     chess::Bitboard getSeenSquares(const chess::Board *board, chess::Color color);
     chess::Bitboard getControlledSquares(const chess::Board *board, chess::Color color);
+    chess::Bitboard getEnemySeenSquaresIgnoringOwnPieces(const Board *board, Color enemyColor);
     
 };
 
