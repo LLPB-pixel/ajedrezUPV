075729b6542c380edbe19e9ec7a3c51991cacbf8
LLPB-pixel <141754913+LLPB-pixel@users.noreply.github.com>
2025-04-18 12:43:21 +0200

    Update GeneralEvaluator.h

--- jorge/GeneralEvaluator.h
+++ jorge/GeneralEvaluator.h
@@ -1,24 +1,26 @@
 #ifndef GENERALEVALUATOR_H
 #define GENERALEVALUATOR_H
 
+#include "chess.hpp"
 #include "Evaluator.h"
 using namespace chess;
+
+
+
 class GeneralEvaluator : public Evaluator {
 public:
-  GeneralEvaluator();
+    GeneralEvaluator();
+    float evaluate(const Board *board, const Color color) override;
+    float positionOfThePiecesAndMaterial(const Board *board) override;
+    float pawn_structure(const Board *board, const Color color) override;
+    float safe_king(const Board *board, const Color color) override;
+    float control(const Board *board, const Color color) override;
+    ~GeneralEvaluator() override;
 
-  float evaluate(const Board *board, const Color color) override;
-  float positionOfThePiecesAndMaterial(const chess::Board *board) override;
-  float pawn_structure(const Board *board, const Color color) override;
-  float safe_king(const Board *board, const Color color) override;
-  float control(const Board *board, const Color color) override;
-
-  ~GeneralEvaluator() override;
-  private:
+private:
     chess::Bitboard getSeenSquares(const chess::Board *board, chess::Color color);
     chess::Bitboard getControlledSquares(const chess::Board *board, chess::Color color);
     chess::Bitboard getEnemySeenSquaresIgnoringOwnPieces(const Board *board, Color enemyColor);
-    
 };
 
 #endif
