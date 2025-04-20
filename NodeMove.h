29f284de8ff426504bf62775fa523fee3d0e2abb
LLPB-pixel <141754913+LLPB-pixel@users.noreply.github.com>
2025-04-20 20:52:12 +0200

    Update NodeMove.h

--- NodeMove.h
+++ NodeMove.h
@@ -2,35 +2,70 @@
 #define NODEMOVE_H
 
 #include "chess.hpp"
-using namespace chess;
+#include "GeneralEvaluator.h"
+#include <array>
+#include <memory>
 
-#include <iostream>
-#include <thread>
-#include <mutex>
-#include <vector>
+namespace chess {
 
-#define MAX_DEPTH 10 //tocar esto para cambiar la profundidad maxima de busqueda
-#define MAX_BRANCH 100
+constexpr int MAX_DEPTH = 4;
+constexpr int MAX_BRANCH = 100;
 
+class NodeMove {
+private:
+    // Core data members
+    int current_depth_;
+    Board board_;
+    NodeMove* parent_;
+    Move last_move_;
+    float eval_;
+    
+    // Child storage
+    std::array<std::unique_ptr<NodeMove>, MAX_BRANCH> children_;
+    size_t child_count_ = 0;
 
-class NodeMove{
-    private:
-        int currentDepth;
-        Board board;
-        NodeMove *parent = nullptr;
-        NodeMove *childs[MAX_BRANCH] = {nullptr};
-        int childIndex = 0;
+public:
+    // Construction
+    NodeMove(Board board, NodeMove* parent = nullptr);
+    ~NodeMove() = default;
+    
+    // 
+    void addChild(Board board, Move move);
+    
+    // Funciones varias para depuracion
+    void printEvaluationsOfChildren() const;
+    void printBoardsAndEvaluationsOfChildren() const;
+    void printBoard() const;
+    int evaluateBoard() const;
+    float minimax(GeneralEvaluator* evaluator, Color root_color);
+    chess::Move getBestMove(float best_score) const;
+    
+    // Getters varios
+    float getEval() const { 
+        return eval_; 
+    }
+    size_t getChildCount() const { 
+        return child_count_; 
+    }
 
-    public:
-        //Constructor genral
-        NodeMove(Board board, NodeMove *parent);
-        // Destructor
-        ~NodeMove();
-        // Añadir hijo
-        void addChild(NodeMove *child);
-        void printBoard();
-        int evaluateBoard();
-        float minimax(GeneralEvaluator *evaluator);
+    NodeMove* getChild(size_t index) const { 
+        return (index < child_count_) ? children_[index].get() : nullptr; 
+    }
+    chess::Move getLastMove() const { 
+        return last_move_; 
+    }
+    int getCurrentDepth() const { 
+        return current_depth_; 
+    }
+
+    // si copias un unique ptr puede hacer pum
+    NodeMove(const NodeMove&) = delete;
+    NodeMove& operator=(const NodeMove&) = delete;
+    
+    // 
+    NodeMove(NodeMove&&) = default;
+    NodeMove& operator=(NodeMove&&) = default;
 };
 
+} // namespace chess
 #endif // NODEMOVE_H
