302c0f04909550d569a36f2c608813284b1cc4b0
LLPB-pixel <141754913+LLPB-pixel@users.noreply.github.com>
2025-04-20 17:46:18 +0200

    Delete classEvaluator.hpp

--- classEvaluator.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "chess.hpp"
-using namespace chess;
-class Evaluator {
-public:
-    virtual float evaluate(const Board *board, Color color) = 0;
-    virtual ~Evaluator() = default;
-};
