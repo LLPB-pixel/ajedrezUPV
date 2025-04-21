0c26065dfc441e5043e1efb5df149c25973184d1
LLPB-pixel <141754913+LLPB-pixel@users.noreply.github.com>
2025-04-21 22:31:34 +0200

    Update EndgameEvaluator.h

--- implementaciones/EndgameEvaluator.h
+++ implementaciones/EndgameEvaluator.h
@@ -7,18 +7,10 @@ class EndgameEvaluator : public GeneralEvaluator {
 public:
     EndgameEvaluator();
     float evaluate(const Board *board, const Color color) override;
-    float positionOfThePiecesAndMaterial(const Board *board);
-    float pawn_structure(const Board *board, const Color color);
-    float safe_king(const Board *board, const Color color);
-    float control(const Board *board, const Color color);
+    float positionOfThePiecesAndMaterial(const Board *board) override;
     ~EndgameEvaluator();
 
 private:
-    chess::Bitboard getSeenSquares(const chess::Board *board, chess::Color color);
-    chess::Bitboard getControlledSquares(const chess::Board *board, chess::Color color);
-    chess::Bitboard getEnemySeenSquaresIgnoringOwnPieces(const Board *board, Color enemyColor);
-    float evaluatePieceType(const Board* board, chess::PieceType type, chess::Color color, float baseValue, const float importance[8][8]);
-
     // Mapas de calor específicos para finales
     static constexpr float king_endgame_heatmap[8][8] = {
         {0.0,  0.1,  0.2,  0.3,  0.3,  0.2,  0.1,  0.0},
